443adf1170e8de5bb2db0717b7cdd365c64e02ab
BiancaGallicchio <94980037+BiancaGallicchio@users.noreply.github.com>
2022-05-28 16:48:00 -0300

    Update soma_matriz.cpp

--- soma_matriz.cpp
+++ soma_matriz.cpp
@@ -1,37 +1,54 @@
 #include <iostream>
 using namespace std;
-#define m 7
-#define n 6
-//calculating sum of each row
-void calcMes(int arr[m][n]){
-   int i,j,soma = 0;
-   for (i = 0; i < 4; ++i) {
-      for (j = 0; j < 4; ++j) {
-         soma = soma + arr[i][j];
+#define m 4
+#define n 5
+//soma de cada linha
+void calcLin(int arr[m][n])
+{
+   int i, j, soma = 0;
+   for (i = 0; i < m; ++i) // linha
+   {
+      for (j = 0; j < n; ++j) // coluna
+      {
+         soma = soma + arr[i][j]; // faz [0][0] + [0][1] + [0][2] + [0][3], [1][0] +[1][1] + ... e assim por diante
       }
-      cout << "Soma "<< i << ": " << soma << endl;
+      cout << "Soma da linha "<< i+1 << ": " << soma << endl;
+      soma = 0; // zerar a soma toda vez que desce pra próx linha
+   }
+}
+//Soma de cada coluna
+void calcCol(int arr[m][n]) 
+{
+   int i, j, soma = 0;
+   for (i = 0; i < n; ++i) 
+   {
+      for (j = 0; j < m; ++j) 
+      {
+         soma = soma + arr[j][i]; // !!! [j][i]
+      }
+      cout << "Soma da coluna "<< i+1 << ": " << soma << endl;
       soma = 0;
    }
 }
-//calculating sum of each column
-void calc_csum(int arr[m][n]) {
-   int i,j,sum = 0;
-   for (i = 0; i < 4; ++i) {
-      for (j = 0; j < 4; ++j) {
-         sum = sum + arr[j][i];
-      }
-      cout << "Sum of the column "<< i << ": " << sum <<endl;
-      sum = 0;
-   }
-}
-int main() {
+int main() 
+{
    int i,j;
    int arr[m][n];
-   int x = 1;
-   for (i = 0; i < m; i++)
-   for (j = 0; j < n; j++)
-   arr[i][j] = x++;
-   calcMes(arr);
-   calc_csum(arr);
+   int x = 10;
+   for (i = 0; i < m; i++) 
+   {
+      cout << " |  ";
+      for (j = 0; j < n; j++) 
+      {
+         arr[i][j] = x++;
+         cout << arr[i][j] << " ";
+      }
+      cout << " | \n";
+   }
+   cout << endl;
+   calcLin(arr);
+   cout << endl;
+   calcCol(arr);
+
    return 0;
 }
